618ebe2da8c2c146a829c44cfe393ee443577287
Arthurap52 <josearthurantunespereira@gmail.com>
2020-06-09 21:54:22 -0300

    update

--- v4.oop/src/config/config.php
+++ v4.oop/src/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-//if (!isset($magic) or !defined($magic)) {
-//  http_response_code(404);
-//  die("404 Not Found");
-//};
+if (!isset($magic) or !defined($magic)) {
+  http_response_code(404);
+  die("404 Not Found");
+};
 
 if (!_LOCAL_APP) {
   define("DB_USER", MYSQL_USER);
